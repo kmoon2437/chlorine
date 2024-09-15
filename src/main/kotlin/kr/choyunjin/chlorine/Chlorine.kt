77435e6f12864cbdd413374445324897139c58ef
kmoon2437 <kmoon2437@gmail.com>
2024-09-16 06:27:13 +0900

    예외처리 부분 제거

--- src/main/kotlin/kr/choyunjin/chlorine/Chlorine.kt
+++ src/main/kotlin/kr/choyunjin/chlorine/Chlorine.kt
@@ -1,6 +1,5 @@
 package kr.choyunjin.chlorine
 
-import java.io.IOException
 import org.bukkit.plugin.java.JavaPlugin
 import org.json.JSONObject
 import org.json.JSONTokener
@@ -16,30 +15,23 @@ class Chlorine : JavaPlugin() {
 
         // 설정파일 생성
         this.saveDefaultConfig()
-        
+
         // 번역 파일 세팅
         val langName = this.config.getString("lang")
-        val langFileName = StringBuilder("translation_")
-        langFileName.append(langName)
-        langFileName.append(".json")
+        val langFileName = "translation_${langName}.json"
+        var langFileData = this.getResource(langFileName)
 
-        try {
-            var langFileData = this.getResource(langFileName.toString())
-
-            if (langFileData == null) {
-                logger.warn("Language \"{}\" not found, falling back to \"en\" (English)", langName)
-                logger.info("Loading language \"en\"")
-                langFileData = this.getResource("translation_en.json")!!
-            } else {
-                logger.info("Loading language \"{}\"", langName)
-            }
-
-            this.i18n = I18n(JSONObject(JSONTokener(langFileData)))
-            langFileData.close()
-        } catch (e: IOException) {
-            throw RuntimeException(e)
+        if (langFileData == null) {
+            logger.warn("Language \"{}\" not found, falling back to \"en\" (English)", langName)
+            logger.info("Loading language \"en\"")
+            langFileData = this.getResource("translation_en.json")!!
+        } else {
+            logger.info("Loading language \"{}\"", langName)
         }
-        
+
+        this.i18n = I18n(JSONObject(JSONTokener(langFileData)))
+        langFileData.close()
+
         // event listener 추가
         this.server.pluginManager.registerEvents(ChatListener(this), this)
         this.server.pluginManager.registerEvents(PlayerJoinQuitListener(this), this)
